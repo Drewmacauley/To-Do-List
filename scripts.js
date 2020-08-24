ba766a28ba5f83e1cf21a911220907b4cb268968
Drewmacauley <macauleyandy03@gmail.com>
2020-08-24 15:42:57 -0700

    add script
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,12 +1,49 @@
+//Business Logic for To-DO List
+function ToDoList() {
+  this.tasks = [];
+  this.currentId = 0
+}
+
+ToDoList.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+ToDoList.prototype.addTask = function(task) {
+  this.tasks.push(task);
+}
+
+ToDoList.prototype.findTask = function(id) {
+  for (i=0; i< this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id == id) {
+        return this.tasks[i];
+      }
+    }
+  };
+  return false;
+}
+
+ToDoList.prototype.deleteTask = function(id) {
+  for (i=0; i< this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id == id) {
+        delete this.tasks[i];
+      }
+    }
+  };
+  return false;
+}
+
+function Task(chore, location, jobSpecific) {
+  this.chore = chore;
+  this.location = location;
+  this.jobSpecific = jobSpecific;
+}
+
+Task.prototype.jobName = function() {
+  return this.chore + " " + jobSpecific;
+}
 
 
-
-
-
-
-
-//Task 
-task Complete
-delete task 
-demonstrate and create specifications with example input and output
 
